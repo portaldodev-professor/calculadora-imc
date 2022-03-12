eb45c92be5a540b9b62ee4fd4f98e6d78492007d
portaldodev-professor <101436777+portaldodev-professor@users.noreply.github.com>
2022-03-11 21:55:50 -0300

    Add files via upload
    Esse projeto foi desenvolvido com as seguintes tecnologias:
    
    -React Native
    -Expo

---
+++ node/app.js
@@ -0,0 +1,56 @@
+function soma(a,b){
+    return a + b
+}
+
+function mult(a, b){
+    return a * b
+}
+
+function sub(a, b){
+    return a - b
+}
+
+function div(a, b){
+    return a / b
+}
+
+console.log(soma(10, 20))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var mostrarSite = true
+const site = "www.guiadoprogramador.com"
+
+console.log("Hello World!")
+console.log("Meu nome é Eduardo")
+console.log("E eu estou aprendendo Node.js com o Guia do Programador")
+
+if(mostrarSite){
+    console.log(site)
+}
+*/
